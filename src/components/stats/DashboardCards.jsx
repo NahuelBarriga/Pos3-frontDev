70d76800901c0debf492e722220dd2a05489146c
Nahuel <nahuel.barriga.nb@gmail.com>
2025-07-14 17:28:38 -0300

    a

--- src/components/stats/DashboardCards.jsx
+++ src/components/stats/DashboardCards.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Calendar, Users, ShoppingBag, Coffee } from 'lucide-react';
 
-const DashboardCards = ({ loading, dashboardStats}) => {
+const DashboardCards = ({ loading, dashboardStats }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
       {/* Pedidos */}
@@ -18,18 +18,18 @@ const DashboardCards = ({ loading, dashboardStats}) => {
           ) : (
             <>
               <span className="text-3xl font-bold text-gray-900">{dashboardStats?.totalOrdersToday}</span>
-              {dashboardStats?.totalOrdersYesterday > 0 && dashboardStats?.totalOrdersToday > 0 && ( 
-                    dashboardStats?.totalOrdersToday/dashboardStats?.totalOrdersYesterday >= 0 ? (
-                        <span className="ml-2 text-sm text-green-500">{(((dashboardStats?.totalOrdersToday - dashboardStats?.totalOrdersYesterday)/dashboardStats?.totalOrdersYesterday)*100).toFixed(1)}% mas que ayer</span>
-                    ) : ( 
-                        <span className="ml-2 text-sm text-red-500">{(((dashboardStats?.totalOrdersYesterday - dashboardStats?.totalOrdersToday)/dashboardStats?.totalOrdersYesterday)*100).toFixed(1)}% menos que ayer</span>
-                    )
+              {dashboardStats?.totalOrdersYesterday > 0 && dashboardStats?.totalOrdersToday > 0 && (
+                dashboardStats?.totalOrdersToday / dashboardStats?.totalOrdersYesterday >= 0 ? (
+                  <span className="ml-2 text-sm text-green-500">{(((dashboardStats?.totalOrdersToday - dashboardStats?.totalOrdersYesterday) / dashboardStats?.totalOrdersYesterday) * 100).toFixed(1)}% mas que ayer</span>
+                ) : (
+                  <span className="ml-2 text-sm text-red-500">{(((dashboardStats?.totalOrdersYesterday - dashboardStats?.totalOrdersToday) / dashboardStats?.totalOrdersYesterday) * 100).toFixed(1)}% menos que ayer</span>
+                )
               )}
             </>
           )}
         </div>
       </div>
-      
+
       {/* Mesas ocupadas */}
       <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200">
         <div className="flex items-center justify-between mb-4">
@@ -46,12 +46,14 @@ const DashboardCards = ({ loading, dashboardStats}) => {
               <span className="text-3xl font-bold text-gray-900">
                 {dashboardStats?.totalTablesOccupied}/{dashboardStats?.totalTables}
               </span>
-              <span className="ml-2 text-sm text-gray-500">{((dashboardStats?.totalTablesOccupied/dashboardStats?.totalTables)*100).toFixed(1)}% ocupación</span>
+              {dashboardStats?.totalTables > 0 && dashboardStats?.totalTablesOccupied > 0 && (
+                <span className="ml-2 text-sm text-gray-500">{((dashboardStats?.totalTablesOccupied / dashboardStats?.totalTables) * 100).toFixed(1)}% ocupación</span>
+              )}
             </>
           )}
         </div>
       </div>
-      
+
       {/* Item más popular */}
       <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200">
         <div className="flex items-center justify-between mb-4">
@@ -65,17 +67,19 @@ const DashboardCards = ({ loading, dashboardStats}) => {
             <span className="text-3xl font-bold text-gray-300">...</span>
           ) : (
             <>
-                <div>  
-                    <span className="text-3xl font-bold text-gray-900">{dashboardStats?.mostPopularItem?.name}</span>
-                    <span className="ml-2 text-sm text-gray-500">#{dashboardStats?.mostPopularItem?.SKU}</span>
-                </div>
-                
-                <span className="text-sm text-gray-500">{dashboardStats?.mostPopularItem?.count} pedidos hoy</span>
+              <div>
+                <span className="text-3xl font-bold text-gray-900">{dashboardStats?.mostPopularItem?.name}</span>
+                {dashboardStats?.mostPopularItem?.SKU && (
+                  <span className="ml-2 text-sm text-gray-500">#{dashboardStats?.mostPopularItem?.SKU}</span>
+                )}
+              </div>
+
+              <span className="text-sm text-gray-500">{dashboardStats?.mostPopularItem?.count} pedidos hoy</span>
             </>
           )}
         </div>
       </div>
-      
+
       {/* Reservas */}
       <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200">
         <div className="flex items-center justify-between mb-4">
@@ -94,7 +98,7 @@ const DashboardCards = ({ loading, dashboardStats}) => {
                 <div className="ml-2 text-sm text-gray-500">proxima: {dashboardStats.nextReservationTime}</div>
               )}
             </div>
-            
+
           )}
         </div>
       </div>
